1b66ff7779f59861d8b465a69d6fc9fbb5f65915
Muhammed Shendi <muhammedshendi@gmail.com>
2021-06-07 11:28:08 +0200

    routes configured with auth and file upload

--- routes/users.js
+++ routes/users.js
@@ -4,30 +4,42 @@ const router = express.Router();
 const multer = require('multer');
 const auth = require("../helpers/auth");
 
+// SetUp multer
+
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, "uploads");
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+const upload = multer({storage: storage});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
-router.get('/users', async(req, res , next) => {
-  await userController.getAll(req,res)
+router.get('/users', async (req, res, next) => {
+    await userController.getAll(req, res)
 });
 
-router.post('/users', async(req, res, next) => {
-  await userController.createUser(req,res)
+router.post('/users', upload.single("file"), async (req, res, next) => {
+    await userController.createUser(req, res)
 });
 
-router.get('/users/:id', async(req, res, next) => {
-  await userController.getUserById(req, res);
+router.get('/users/:id', async (req, res, next) => {
+    await userController.getUserById(req, res);
 })
 
-router.put('/users/:id', async(req, res, next) => {
-  await userController.updateUser(req, res);
+router.put('/users/:id', auth, async (req, res, next) => {
+    await userController.updateUser(req, res);
 })
 
-router.delete('/users/:id' , async(req, res, next) => {
-  await userController.deleteUser(req,res)
+router.delete('/users/:id', auth, async (req, res, next) => {
+    await userController.deleteUser(req, res)
 });
 
 
