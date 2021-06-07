8d2927f9b79c0dbe9711f2e57cc3aa92d971984a
Muhammed Shendi <muhammedshendi@gmail.com>
2021-06-07 21:23:00 +0200

    added login and logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,67 +1,118 @@
-const User = require("../models/User")
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
-const getAll = async(req,res,next) => {
-    try{
-        const users = await User.find({});
-        return res.status(200).json(users);
-    }
-    catch(error){
-        console.log(error);
-        return res.status(400).send('error in get users');
-    }
-} 
+const getAll = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("error in get users");
+  }
+};
 
-const createUser = async(req,res,next) => {
-    const user = req.body;
-    try{
-        await User.create(user);
-        return res.status(200).send('create user successfully');
-    }
-    catch(error){
-        console.log(error);
-        return res.status(400).send('error in create user');
-    }
-}
+const createUser = async (req, res, next) => {
+  const user = req.body;
+  try {
+    await User.create(user);
+    return res.status(200).send("create user successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("error in create user");
+  }
+};
 
-const getUserById = async(req, res, next) => {
-    const id = req.params.id.toString();
-    try {
-        const user = await User.findById(id);
-        return res.status(200).send(user)
-    } catch (error) {
-        console.error(error);
-        return res.status(400).send("User not found");
-    }
-}
+const getUserById = async (req, res, next) => {
+  const id = req.params.id.toString();
+  try {
+    const user = await User.findById(id);
+    return res.status(200).send(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("User not found");
+  }
+};
 
 const updateUser = async (req, res, next) => {
-    const id = req.params.id.toString();
-    try {
-        await User.findOneAndUpdate({_id: id}, req.body);
-        return res.status(200).send('Updated Successfully')
-    } catch (error) {
-        console.error(error);
-        return res.status(400).send('Error encountred');
+  const id = req.params.id.toString();
+  try {
+    await User.findOneAndUpdate({ _id: id }, req.body);
+    return res.status(200).send("Updated Successfully");
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("Error encountred");
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.remove();
+      return res.status(200).json({ message: "Delete user done" });
     }
-}
+    return res.status(400).json({ message: "Error deleting user" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("error in delete user");
+  }
+};
 
-const deleteUser = async(req,res,next) => {
-    try{
-        const user = await User.findById(req.params.id);
-        if (user){
-            await user.remove();
-            return res.status(200).json({message: "Delete user done"});
-        }
-        return res.status(400).json({message: "Error deleting user"});
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const data = {};
+  console.log(req.body);
 
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({ error: "invalid credentials" });
     }
-    catch(error){
-        console.log(error);
-        return res.status(400).send('error in delete user')
-    }
-}
+    bcrypt.compare(password, user.password, (err, matched) => {
+      if (matched) {
+        data.userId = user.id;
+        data.username = user.firstname + " " + user.lastname;
+        data.email = user.email;
+        data.created_at = user.created_at;
 
+        const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY);
+        const expirationTime = new Date(
+          Date.now() + parseInt(process.env.JWT_EXPIRATION)
+        );
 
+        res.cookie("token", token, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
 
-module.exports = {createUser , getAll ,getUserById,updateUser, deleteUser}
+        res.cookie("email", data.email, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
 
+        res.cookie("userId", data.userId, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
+
+        return res.status(200).json({ ...data });
+      }
+      return res.status(401).json({ error: "Invalid credentials" });
+    });
+  } catch (err) {
+    res.status(401).json({
+      error: "Error logging you in, please try again later",
+    });
+  }
+};
+
+module.exports = {
+  createUser,
+  getAll,
+  getUserById,
+  updateUser,
+  deleteUser,
+  login,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,55 @@
-const userController = require("../controllers/userController")
-const express = require('express');
+const userController = require("../controllers/userController");
+const express = require("express");
 const router = express.Router();
-const multer = require('multer');
+const multer = require("multer");
 const auth = require("../helpers/auth");
 
 // SetUp multer
 
 const storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, "uploads");
-    },
-    filename: (req, file, callBack) => {
-        callBack(null, Date.now() + "-" + file.originalname);
-    },
+  destination: (req, file, callBack) => {
+    callBack(null, "uploads");
+  },
+  filename: (req, file, callBack) => {
+    callBack(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/users', async (req, res, next) => {
-    await userController.getAll(req, res)
+router.get("/users", async (req, res, next) => {
+  await userController.getAll(req, res);
 });
 
-router.post('/users', upload.single("file"), async (req, res, next) => {
-    await userController.createUser(req, res)
+router.post("/users", upload.single("file"), async (req, res, next) => {
+  await userController.createUser(req, res);
 });
 
-router.get('/users/:id', async (req, res, next) => {
-    await userController.getUserById(req, res);
-})
-
-router.put('/users/:id', auth, async (req, res, next) => {
-    await userController.updateUser(req, res);
-})
-
-router.delete('/users/:id', auth, async (req, res, next) => {
-    await userController.deleteUser(req, res)
+router.post("/users/login", async (req, res, next) => {
+  await userController.login(req, res);
 });
 
+router.post("/users/logout", auth, async (req, res, next) => {
+  res.clearCookie("token");
+  res.clearCookie("user_id");
+  res.clearCookie("username");
+});
+
+router.get("/users/:id", async (req, res, next) => {
+  await userController.getUserById(req, res);
+});
+
+router.put("/users/:id", auth, async (req, res, next) => {
+  await userController.updateUser(req, res);
+});
+
+router.delete("/users/:id", auth, async (req, res, next) => {
+  await userController.deleteUser(req, res);
+});
 
 module.exports = router;
