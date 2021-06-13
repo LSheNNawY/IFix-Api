3e752a6b243546d74967243ae39fc2aca53e083d
LSheNNawY <devshennawy@gmail.com>
2021-06-13 21:19:39 +0200

    editing user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,6 +43,7 @@ const createUser = async (req, res) => {
 
   const { error } = userValidation.validate(req.body);
   if (error) {
+    console.log(error);
     return res.status(400).send(error.details[0].message);
   }
 
