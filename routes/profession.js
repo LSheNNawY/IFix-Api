ecd7ab1bd2f67301c568e7bbd18fc75b8acca80b
IslamAhmed0 <ea43533@gmail.com>
2021-06-17 01:12:16 +0200

    edit update proffiesion

--- routes/profession.js
+++ routes/profession.js
@@ -21,14 +21,15 @@ const upload = multer({storage:fileStorage})
 
 professionRouter.get('/', function(req, res, next) {
     res.send('respond with a resource');
-  });
+});
 
 professionRouter.get('/professions', async function (req, res, next) {
     await professionController.getAll(req, res);
 });
 
 professionRouter.post('/professions',upload.single("img"), async (req, res,next) => {
-   console.log(req.file)
+
+    console.log(req.file)
     console.log(req.body)
 
     await professionController.createProfession(req, res);
@@ -38,7 +39,7 @@ professionRouter.get('/professions/:id', async function (req, res, next) {
     await professionController.getProfessionById(req, res);
 });
 
-professionRouter.put('/professions/:id', async function (req, res, next) {
+professionRouter.put('/professions/:id',upload.single("img"), async function (req, res, next) {
     await professionController.updateProfession(req, res);
 });
 
