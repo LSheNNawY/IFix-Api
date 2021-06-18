a4ab90f86f49ddb5fb9b14ebed972b287fe397ce
IslamAhmed0 <ea43533@gmail.com>
2021-06-18 19:59:49 +0200

    edit employee controller

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -61,7 +61,7 @@ const createEmployee = async (req, res) => {
 const getEmployeeById = async (req, res) => {
     const id = req.params.id.toString();
     try {
-        const employee = await User.findById(id);
+        const employee = await User.findById(id).populate('profession');
         return res.status(200).json(employee);
     } catch (error) {
         console.error(error);
