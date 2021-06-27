8dc212c25a702a71b91aacea0a8c08fa0b26d28c
Muhammed Shendi <muhammedshendi@gmail.com>
2021-06-27 19:23:35 +0200

    store user role in cookies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const {mail} = require("../helpers/mail");
+const { mail } = require("../helpers/mail");
 const jwt = require("jsonwebtoken");
 const userValidation = require("../helpers/userValidation");
 const Job = require("../models/Job");
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 /**
  * get all users function
@@ -14,13 +14,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
  */
 
 const getAll = async (req, res) => {
-    try {
-        const users = await User.find({role: "user"});
-        return res.status(200).json(users);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send("error in get users");
-    }
+  try {
+    const users = await User.find({ role: "user" });
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("error in get users");
+  }
 };
 
 /**
@@ -31,271 +31,276 @@ const getAll = async (req, res) => {
  */
 
 const createUser = async (req, res) => {
-    const {firstName, lastName, email, password, phone, address, dateOfBirth} =
-        req.body;
-    let picture;
-    if (req.file) picture = req.file.filename;
+  const { firstName, lastName, email, password, phone, address, dateOfBirth } =
+    req.body;
+  let picture;
+  if (req.file) picture = req.file.filename;
 
-    const {error} = userValidation.validate(req.body);
-    if (error) {
-        console.log(error);
-        return res.status(400).send(error.details[0].message);
+  const { error } = userValidation.validate(req.body);
+  if (error) {
+    console.log(error);
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).send("email is already registered");
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = new User({
+    firstName,
+    lastName,
+    email,
+    passwordHash,
+    phone,
+    address,
+    dateOfBirth,
+    picture,
+  });
+  try {
+    const saved = await newUser.save();
+    if (saved) {
+      await mail({
+        from: `IFIX < ${process.env.MAIL_SENDER_EMAIL_ADDRESS} >`,
+        to: email,
+        html: `<h2>You have registered</h2>`,
+        subject: "IFix registeratin",
+      });
+      return res.status(200).send(newUser);
     }
-
-    const userExists = await User.findOne({email});
-    if (userExists) {
-        return res.status(400).send("email is already registered");
-    }
-
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        passwordHash,
-        phone,
-        address,
-        dateOfBirth,
-        picture,
-    });
-    try {
-        const saved = await newUser.save();
-        if (saved) {
-            await mail({
-                from: `IFIX < ${process.env.MAIL_SENDER_EMAIL_ADDRESS} >`,
-                to: email,
-                html: `<h2>You have registered</h2>`,
-                subject: "IFix registeratin",
-            });
-            return res.status(200).send(newUser);
-        }
-    } catch (error) {
-        console.error(error);
-    }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getUserById = async (req, res) => {
-    const id = req.params.id.toString();
-    try {
-        const user = await User.findById(id).populate('jobs');
-        return res.status(200).send(user);
-    } catch (error) {
-        console.error(error);
-        return res.status(400).send("User not found");
-    }
+  const id = req.params.id.toString();
+  try {
+    const user = await User.findById(id).populate("jobs");
+    return res.status(200).send(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("User not found");
+  }
 };
 
 const updateUser = async (req, res) => {
-    const id = req.params.id.toString();
-    const {error} = userValidation.validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    try {
-        await User.findOneAndUpdate({_id: id}, req.body);
-        return res.status(200).send("Updated Successfully");
-    } catch (error) {
-        console.error(error);
-        return res.status(400).send("Error encountred");
-    }
+  const id = req.params.id.toString();
+  const { error } = userValidation.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    await User.findOneAndUpdate({ _id: id }, req.body);
+    return res.status(200).send("Updated Successfully");
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send("Error encountred");
+  }
 };
 
 const blockUser = async (req, res) => {
-    const id = req.params.id.toString();
-    try {
-        await User.findOneAndUpdate({_id: id}, {status: "blocked"});
-        return res.status(200).json({message: "User blocked", status: "blocked"});
-    } catch (error) {
-        console.error(error);
-        return res.status(402).send("Error blocking user");
-    }
+  const id = req.params.id.toString();
+  try {
+    await User.findOneAndUpdate({ _id: id }, { status: "blocked" });
+    return res.status(200).json({ message: "User blocked", status: "blocked" });
+  } catch (error) {
+    console.error(error);
+    return res.status(402).send("Error blocking user");
+  }
 };
 
 const unblockUser = async (req, res) => {
-    const id = req.params.id.toString();
-    try {
-        await User.findOneAndUpdate({_id: id}, {status: "active"});
-        return res
-            .status(200)
-            .json({message: "User unblocked", status: "active"});
-    } catch (error) {
-        console.error(error);
-        return res.status(402).send("Error blocking user");
-    }
+  const id = req.params.id.toString();
+  try {
+    await User.findOneAndUpdate({ _id: id }, { status: "active" });
+    return res
+      .status(200)
+      .json({ message: "User unblocked", status: "active" });
+  } catch (error) {
+    console.error(error);
+    return res.status(402).send("Error blocking user");
+  }
 };
 
 const deleteUser = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            await user.remove();
-            return res.status(200).json({message: "Delete user done"});
-        }
-        return res.status(400).json({message: "Error deleting user"});
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send("error in delete user");
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await user.remove();
+      return res.status(200).json({ message: "Delete user done" });
     }
+    return res.status(400).json({ message: "Error deleting user" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send("error in delete user");
+  }
 };
 
 const login = async (req, res) => {
-    const {email, password} = req.body;
-    const data = {};
-    console.log(req.body);
+  const { email, password } = req.body;
+  const data = {};
+  console.log(req.body);
 
-    try {
-        const user = await User.findOne({email: email});
-        if (!user) {
-            return res.status(401).json({error: "invalid credentials"});
-        }
-        bcrypt.compare(password, user.passwordHash, (err, matched) => {
-            if (matched) {
-                data.userId = user.id;
-                data.username = user.firstName + " " + user.lastName;
-                data.email = user.email;
-                data.role = user.role;
-                data.picture = user.picture;
-                data.created_at = user.created_at;
-
-                const token = jwt.sign({email: user.email}, process.env.SECRET_KEY);
-                const expirationTime = new Date(
-                    Date.now() + parseInt(process.env.JWT_EXPIRATION)
-                );
-
-                res.cookie("token", token, {
-                    httpOnly: true,
-                    expires: expirationTime,
-                });
-
-                res.cookie("email", data.email, {
-                    httpOnly: true,
-                    expires: expirationTime,
-                });
-
-                res.cookie("userId", data.userId, {
-                    httpOnly: true,
-                    expires: expirationTime,
-                });
-
-                return res.status(200).json({...data});
-            }
-            return res.status(401).json({error: "Invalid credentials"});
-        });
-    } catch (err) {
-        res.status(401).json({
-            error: "Error logging you in, please try again later",
-        });
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({ error: "invalid credentials" });
     }
+    bcrypt.compare(password, user.passwordHash, (err, matched) => {
+      if (matched) {
+        data.userId = user.id;
+        data.username = user.firstName + " " + user.lastName;
+        data.email = user.email;
+        data.role = user.role;
+        data.picture = user.picture;
+        data.created_at = user.created_at;
+
+        const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY);
+        const expirationTime = new Date(
+          Date.now() + parseInt(process.env.JWT_EXPIRATION)
+        );
+
+        res.cookie("token", token, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
+
+        res.cookie("email", data.email, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
+
+        res.cookie("userId", data.userId, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
+
+        res.cookie("role", data.role, {
+          httpOnly: true,
+          expires: expirationTime,
+        });
+
+        return res.status(200).json({ ...data });
+      }
+      return res.status(401).json({ error: "Invalid credentials" });
+    });
+  } catch (err) {
+    res.status(401).json({
+      error: "Error logging you in, please try again later",
+    });
+  }
 };
 
 const verifyPassword = async (req, res) => {
-    const {userId, password} = req.body;
-    console.log(req.body);
-    try {
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(400).json({error: "User not found "});
-        }
-        bcrypt.compare(password, user.passwordHash, (err, matched) => {
-            if (matched) {
-                return res.json(true);
-            } else {
-                return res.json(false);
-            }
-        });
-    } catch (err) {
-        console.error(err);
+  const { userId, password } = req.body;
+  console.log(req.body);
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ error: "User not found " });
     }
+    bcrypt.compare(password, user.passwordHash, (err, matched) => {
+      if (matched) {
+        return res.json(true);
+      } else {
+        return res.json(false);
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const isLoggedIn = (req, res) => {
-    try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.json(false);
-        }
-
-        const verifiedToken = jwt.verify(token, process.env.SECRET_KEY);
-        res.send(true);
-    } catch (err) {
-        res.send(false);
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.json(false);
     }
+
+    const verifiedToken = jwt.verify(token, process.env.SECRET_KEY);
+    res.send(true);
+  } catch (err) {
+    res.send(false);
+  }
 };
 
 const logout = (req, res) => {
-    res.cookie("token", "", {
-        httpOnly: true,
-        expires: new Date(0),
-    });
-    res.cookie("username", "", {
-        expires: new Date(0),
-    });
-    res.cookie("userId", "", {
-        expires: new Date(0),
-    });
-    res.send();
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.cookie("username", "", {
+    expires: new Date(0),
+  });
+  res.cookie("userId", "", {
+    expires: new Date(0),
+  });
+  res.cookie("role", "", {
+    expires: new Date(0),
+  });
+  res.send();
 };
 
 const getCurrentUser = async (req, res) => {
-    let user = {};
-    try {
-        const userData = await User.findById(req.cookies.userId)
-        if (userData) {
-            user = {
-                id: userData._id,
-                username: userData.firstName + " " + userData.lastName,
-                email: userData.email,
-                address: userData.address,
-                role: userData.role,
-                picture: userData.picture,
-            };
+  let user = {};
+  try {
+    const userData = await User.findById(req.cookies.userId);
+    if (userData) {
+      user = {
+        id: userData._id,
+        username: userData.firstName + " " + userData.lastName,
+        email: userData.email,
+        address: userData.address,
+        role: userData.role,
+        picture: userData.picture,
+      };
 
-            return res.status(200).json(user);
-        }
-
-        return res.send(undefined)
-
-    } catch (err) {
-        res.send(err);
+      return res.status(200).json(user);
     }
-};
 
+    return res.send(undefined);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 const payment = async (req, res) => {
-    const {amount, job_id, id} = req.body
-    try {
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: "USD",
-            description: "Your Company Description",
-            payment_method: id,
-            confirm: true,
-        });
-        console.log("stripe-routes.js 19 | payment", paymentIntent);
-        const job = await Job.findById(job_id);
-        job.price = amount;
-        job.payment_method = "Credit Card";
-        job.ended_at = new Date();
-        job.save();
-        return res.status(200).json({paymentIntent,job});
-    } catch (err) {
-        res.send(err);
-    }
+  const { amount, job_id, id } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "USD",
+      description: "Your Company Description",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("stripe-routes.js 19 | payment", paymentIntent);
+    const job = await Job.findById(job_id);
+    job.price = amount;
+    job.payment_method = "Credit Card";
+    job.ended_at = new Date();
+    job.save();
+    return res.status(200).json({ paymentIntent, job });
+  } catch (err) {
+    res.send(err);
+  }
 };
 
-
 const sendMailer = async (req, res) => {
-    const {email, body, name} = req.body;
-    let to = `IFIX < ${process.env.MAIL_SENDER_EMAIL_ADDRESS} >`;
-    console.log(to)
-    let subject = "IFix Comment";
-    try {
-        await mail({
-            from: email,
-            to: to,
-            html: `<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+  const { email, body, name } = req.body;
+  let to = `IFIX < ${process.env.MAIL_SENDER_EMAIL_ADDRESS} >`;
+  console.log(to);
+  let subject = "IFix Comment";
+  try {
+    await mail({
+      from: email,
+      to: to,
+      html: `<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
                 <tr>
                 <td style="padding:0;background:#70bbd9;">
                     my name : ${name}
@@ -307,28 +312,27 @@ const sendMailer = async (req, res) => {
                 </td>
                 </tr>
                 </table>`,
-            subject: subject,
-        });
-        return res.status(200).send("done");
-
-    } catch (err) {
-        return res.status(200).send("error");
-    }
-}
+      subject: subject,
+    });
+    return res.status(200).send("done");
+  } catch (err) {
+    return res.status(200).send("error");
+  }
+};
 
 module.exports = {
-    createUser,
-    getAll,
-    getUserById,
-    updateUser,
-    blockUser,
-    unblockUser,
-    deleteUser,
-    login,
-    verifyPassword,
-    isLoggedIn,
-    logout,
-    getCurrentUser,
-    payment,
-    sendMailer
+  createUser,
+  getAll,
+  getUserById,
+  updateUser,
+  blockUser,
+  unblockUser,
+  deleteUser,
+  login,
+  verifyPassword,
+  isLoggedIn,
+  logout,
+  getCurrentUser,
+  payment,
+  sendMailer,
 };
