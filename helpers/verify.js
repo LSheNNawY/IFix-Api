8201155ea7ed0c66bf60d5b397513325c627a829
Muhammed Shendi <muhammedshendi@gmail.com>
2021-06-27 20:04:20 +0200

    verify all users types

--- helpers/verify.js
+++ helpers/verify.js
@@ -11,4 +11,40 @@ module.exports = {
       next();
     }
   },
+  isEmployee: (req, res, next) => {
+    const role = req.cookies.role;
+    if (!role) {
+      return res.status(403).send({ error: "Cookie not found" });
+    } else {
+      if (role !== "employee") {
+        return res.status(403).send({ error: "Unauthorized" });
+      }
+
+      next();
+    }
+  },
+  isUser: (req, res, next) => {
+    const role = req.cookies.role;
+    if (!role) {
+      return res.status(403).send({ error: "Cookie not found" });
+    } else {
+      if (role !== "user") {
+        return res.status(403).send({ error: "Unauthorized" });
+      }
+
+      next();
+    }
+  },
+  isSuperAdmin: (req, res, next) => {
+    const role = req.cookies.role;
+    if (!role) {
+      return res.status(403).send({ error: "Cookie not found" });
+    } else {
+      if (role !== "super admin") {
+        return res.status(403).send({ error: "Unauthorized" });
+      }
+
+      next();
+    }
+  },
 };
