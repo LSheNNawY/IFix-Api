b60874b4ea736398686c709a92799ae39a44a1c1
Muhammed Shendi <muhammedshendi@gmail.com>
2021-07-03 06:46:32 +0200

    admin and superadmin can update picture

--- routes/admins.js
+++ routes/admins.js
@@ -1,7 +1,19 @@
 const adminsController = require("../controllers/adminsController");
 const express = require("express");
 const verify = require("../helpers/verify");
+const multer = require("multer");
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, callBack) => {
+    callBack(null, "./public/uploads/users/");
+  },
+  filename: (req, file, callBack) => {
+    callBack(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/admins", verify.isSuperAdmin, async (req, res, next) => {
   await adminsController.getAllAdmins(req, res);
@@ -15,9 +27,14 @@ router.get("/admins/:id", verify.isSuperAdmin, async (req, res, next) => {
   await adminsController.getAdminById(req, res);
 });
 
-router.put("/admins/:id", verify.isSuperAdmin, async (req, res) => {
-  await adminsController.updateAdmin(req, res);
-});
+router.put(
+  "/admins/:id",
+  verify.isSuperAdmin,
+  upload.single("picture"),
+  async (req, res) => {
+    await adminsController.updateAdmin(req, res);
+  }
+);
 
 router.put("/admins/:id/block", verify.isSuperAdmin, async (req, res, next) => {
   await adminsController.blockAdmin(req, res);
